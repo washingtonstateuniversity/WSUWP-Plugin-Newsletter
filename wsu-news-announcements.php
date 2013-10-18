a486e391c72f3059ad7cfd091051893cd9fedc63
jeremyfelt <jeremy.felt@gmail.com>
2013-10-18 14:10:58 -0700

    Zeroise the day so that the query doesn't fail

--- wsu-news-announcements.php
+++ wsu-news-announcements.php
@@ -224,7 +224,7 @@ class WSU_News_Announcements {
 		}
 
 		if ( isset( $query->query['day'] ) ) {
-			$query_date .= $query->query['day'];
+			$query_date .= zeroise( $query->query['day'], 2 );
 			$query->set( 'day', '' );
 			$query->set( 'posts_per_page', 50 ); // Try to fit all of one day's announcements on a screen.
 		}
