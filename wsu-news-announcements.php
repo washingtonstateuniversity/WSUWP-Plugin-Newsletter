175b938ad0939ffacbcc0e2c06805586e9c44723
jeremyfelt <jeremy.felt@gmail.com>
2013-10-18 13:39:05 -0700

    Code familiarization and cleanup
    Modified code formatting as I read through the code for the first time as this is originally code from WordPress 1.0.0.
    
    Changed the cache key and changed queries to use our post type slug instead of posts.

--- wsu-news-announcements.php
+++ wsu-news-announcements.php
@@ -25,6 +25,11 @@ class WSU_News_Announcements {
 	var $post_type_archive = 'announcements';
 
 	/**
+	 * @var string Key used for storing the announcement calendar in cache.
+	 */
+	var $calendar_cache_key = 'wsu_announcement_calendar';
+
+	/**
 	 * Set up the hooks used by WSU_News_Announcements
 	 */
 	public function __construct() {
@@ -473,94 +478,96 @@ class WSU_News_Announcements {
 	 * @param bool $echo Optional, default is true. Set to false for return.
 	 * @return string|null String when retrieving, null when displaying.
 	 */
-	public function get_calendar($initial = true, $echo = true) {
+	public function get_calendar( $initial = true, $echo = true ) {
+		/* @global WPDB $wpdb */
 		global $wpdb, $m, $monthnum, $year, $wp_locale, $posts;
 
-		$cache = array();
 		$key = md5( $m . $monthnum . $year );
-		if ( $cache = wp_cache_get( 'get_calendar', 'calendar' ) ) {
-			if ( is_array($cache) && isset( $cache[ $key ] ) ) {
+
+		if ( $cache = wp_cache_get( $this->calendar_cache_key, $this->post_type ) ) {
+			if ( is_array( $cache ) && isset( $cache[ $key ] ) ) {
 				if ( $echo ) {
-					echo apply_filters( 'get_calendar',  $cache[$key] );
+					echo $cache[ $key ];
 					return;
 				} else {
-					return apply_filters( 'get_calendar',  $cache[$key] );
+					return $cache[ $key ];
 				}
 			}
 		}
 
-		if ( !is_array($cache) )
+		if ( ! is_array( $cache ) )
 			$cache = array();
 
 		// Quick check. If we have no posts at all, abort!
-		if ( !$posts ) {
-			$gotsome = $wpdb->get_var("SELECT 1 as test FROM $wpdb->posts WHERE post_type = 'post' AND post_status = 'publish' LIMIT 1");
-			if ( !$gotsome ) {
+		if ( ! $posts ) {
+			$gotsome = $wpdb->get_var( $wpdb->prepare( "SELECT 1 as test FROM $wpdb->posts WHERE post_type = %s AND post_status = 'publish' LIMIT 1", $this->post_type ) );
+			if ( ! $gotsome ) {
 				$cache[ $key ] = '';
-				wp_cache_set( 'get_calendar', $cache, 'calendar' );
+				wp_cache_set( $this->calendar_cache_key, $cache, $this->post_type );
 				return;
 			}
 		}
 
 		if ( isset($_GET['w']) )
-			$w = ''.intval($_GET['w']);
+			$w = '' . intval( $_GET['w'] );
 
 		// week_begins = 0 stands for Sunday
-		$week_begins = intval(get_option('start_of_week'));
+		$week_begins = intval( get_option( 'start_of_week' ) );
 
 		// Let's figure out when we are
-		if ( !empty($monthnum) && !empty($year) ) {
-			$thismonth = ''.zeroise(intval($monthnum), 2);
-			$thisyear = ''.intval($year);
-		} elseif ( !empty($w) ) {
+		if ( ! empty( $monthnum ) && ! empty( $year ) ) {
+			$thismonth = '' . zeroise( intval( $monthnum ), 2 );
+			$thisyear  = '' . intval( $year );
+		} elseif ( ! empty( $w ) ) {
 			// We need to get the month from MySQL
-			$thisyear = ''.intval(substr($m, 0, 4));
-			$d = (($w - 1) * 7) + 6; //it seems MySQL's weeks disagree with PHP's
-			$thismonth = $wpdb->get_var("SELECT DATE_FORMAT((DATE_ADD('{$thisyear}0101', INTERVAL $d DAY) ), '%m')");
-		} elseif ( !empty($m) ) {
-			$thisyear = ''.intval(substr($m, 0, 4));
-			if ( strlen($m) < 6 )
+			$thisyear  = '' . intval( substr( $m, 0, 4 ) );
+			$d = ( ( $w - 1 ) * 7 ) + 6; //it seems MySQL's weeks disagree with PHP's
+			$thismonth = $wpdb->get_var( "SELECT DATE_FORMAT((DATE_ADD('{$thisyear}0101', INTERVAL $d DAY) ), '%m')" );
+		} elseif ( ! empty( $m ) ) {
+			$thisyear = '' . intval( substr( $m, 0, 4 ) );
+			if ( strlen( $m ) < 6 )
 				$thismonth = '01';
 			else
-				$thismonth = ''.zeroise(intval(substr($m, 4, 2)), 2);
+				$thismonth = '' . zeroise( intval( substr( $m, 4, 2 ) ), 2 );
 		} else {
-			$thisyear = gmdate('Y', current_time('timestamp'));
-			$thismonth = gmdate('m', current_time('timestamp'));
+			$thisyear  = gmdate( 'Y', current_time( 'timestamp' ) );
+			$thismonth = gmdate( 'm', current_time( 'timestamp' ) );
 		}
 
-		$unixmonth = mktime(0, 0 , 0, $thismonth, 1, $thisyear);
-		$last_day = date('t', $unixmonth);
+		$unixmonth = mktime( 0, 0 , 0, $thismonth, 1, $thisyear );
+		$last_day  = date( 't', $unixmonth );
 
 		// Get the next and previous month and year with at least one post
-		$previous = $wpdb->get_row("SELECT MONTH(post_date) AS month, YEAR(post_date) AS year
+		$previous = $wpdb->get_row( $wpdb->prepare( "SELECT MONTH(post_date) AS month, YEAR(post_date) AS year
 		FROM $wpdb->posts
 		WHERE post_date < '$thisyear-$thismonth-01'
-		AND post_type = 'post' AND post_status = 'publish'
+		AND post_type = %s AND post_status = 'publish'
 			ORDER BY post_date DESC
-			LIMIT 1");
-		$next = $wpdb->get_row("SELECT MONTH(post_date) AS month, YEAR(post_date) AS year
+			LIMIT 1", $this->post_type ) );
+
+		$next = $wpdb->get_row( $wpdb->prepare( "SELECT MONTH(post_date) AS month, YEAR(post_date) AS year
 		FROM $wpdb->posts
 		WHERE post_date > '$thisyear-$thismonth-{$last_day} 23:59:59'
-		AND post_type = 'post' AND post_status = 'publish'
+		AND post_type = %s AND post_status = 'publish'
 			ORDER BY post_date ASC
-			LIMIT 1");
+			LIMIT 1", $this->post_type ) );
 
 		/* translators: Calendar caption: 1: month name, 2: 4-digit year */
-		$calendar_caption = _x('%1$s %2$s', 'calendar caption');
+		$calendar_caption = _x( '%1$s %2$s', 'calendar caption' );
 		$calendar_output = '<table id="wp-calendar">
-	<caption>' . sprintf($calendar_caption, $wp_locale->get_month($thismonth), date('Y', $unixmonth)) . '</caption>
+	<caption>' . sprintf( $calendar_caption, $wp_locale->get_month( $thismonth ), date( 'Y', $unixmonth ) ) . '</caption>
 	<thead>
 	<tr>';
 
 		$myweek = array();
 
 		for ( $wdcount=0; $wdcount<=6; $wdcount++ ) {
-			$myweek[] = $wp_locale->get_weekday(($wdcount+$week_begins)%7);
+			$myweek[] = $wp_locale->get_weekday( ( $wdcount + $week_begins ) % 7 );
 		}
 
 		foreach ( $myweek as $wd ) {
-			$day_name = (true == $initial) ? $wp_locale->get_weekday_initial($wd) : $wp_locale->get_weekday_abbrev($wd);
-			$wd = esc_attr($wd);
+			$day_name = ( true == $initial ) ? $wp_locale->get_weekday_initial( $wd ) : $wp_locale->get_weekday_abbrev( $wd );
+			$wd = esc_attr( $wd );
 			$calendar_output .= "\n\t\t<th scope=\"col\" title=\"$wd\">$day_name</th>";
 		}
 
@@ -572,7 +579,7 @@ class WSU_News_Announcements {
 	<tr>';
 
 		if ( $previous ) {
-			$calendar_output .= "\n\t\t".'<td colspan="3" id="prev"><a href="' . get_month_link($previous->year, $previous->month) . '" title="' . esc_attr( sprintf(__('View posts for %1$s %2$s'), $wp_locale->get_month($previous->month), date('Y', mktime(0, 0 , 0, $previous->month, 1, $previous->year)))) . '">&laquo; ' . $wp_locale->get_month_abbrev($wp_locale->get_month($previous->month)) . '</a></td>';
+			$calendar_output .= "\n\t\t".'<td colspan="3" id="prev"><a href="' . get_month_link( $previous->year, $previous->month ) . '" title="' . esc_attr( sprintf(__('View posts for %1$s %2$s'), $wp_locale->get_month( $previous->month ), date( 'Y', mktime( 0, 0 , 0, $previous->month, 1, $previous->year ) ) ) ) . '">&laquo; ' . $wp_locale->get_month_abbrev( $wp_locale->get_month( $previous->month ) ) . '</a></td>';
 		} else {
 			$calendar_output .= "\n\t\t".'<td colspan="3" id="prev" class="pad">&nbsp;</td>';
 		}
@@ -580,7 +587,7 @@ class WSU_News_Announcements {
 		$calendar_output .= "\n\t\t".'<td class="pad">&nbsp;</td>';
 
 		if ( $next ) {
-			$calendar_output .= "\n\t\t".'<td colspan="3" id="next"><a href="' . get_month_link($next->year, $next->month) . '" title="' . esc_attr( sprintf(__('View posts for %1$s %2$s'), $wp_locale->get_month($next->month), date('Y', mktime(0, 0 , 0, $next->month, 1, $next->year))) ) . '">' . $wp_locale->get_month_abbrev($wp_locale->get_month($next->month)) . ' &raquo;</a></td>';
+			$calendar_output .= "\n\t\t".'<td colspan="3" id="next"><a href="' . get_month_link( $next->year, $next->month ) . '" title="' . esc_attr( sprintf(__('View posts for %1$s %2$s'), $wp_locale->get_month( $next->month ), date( 'Y', mktime( 0, 0 , 0, $next->month, 1, $next->year ) ) ) ) . '">' . $wp_locale->get_month_abbrev( $wp_locale->get_month( $next->month ) ) . ' &raquo;</a></td>';
 		} else {
 			$calendar_output .= "\n\t\t".'<td colspan="3" id="next" class="pad">&nbsp;</td>';
 		}
@@ -593,10 +600,11 @@ class WSU_News_Announcements {
 	<tr>';
 
 		// Get days with posts
-		$dayswithposts = $wpdb->get_results("SELECT DISTINCT DAYOFMONTH(post_date)
+		$dayswithposts = $wpdb->get_results( $wpdb->prepare( "SELECT DISTINCT DAYOFMONTH(post_date)
 		FROM $wpdb->posts WHERE post_date >= '{$thisyear}-{$thismonth}-01 00:00:00'
-		AND post_type = 'post' AND post_status = 'publish'
-		AND post_date <= '{$thisyear}-{$thismonth}-{$last_day} 23:59:59'", ARRAY_N);
+		AND post_type = %s AND post_status = 'publish'
+		AND post_date <= '{$thisyear}-{$thismonth}-{$last_day} 23:59:59'", $this->post_type ), ARRAY_N );
+
 		if ( $dayswithposts ) {
 			foreach ( (array) $dayswithposts as $daywith ) {
 				$daywithpost[] = $daywith[0];
@@ -605,72 +613,73 @@ class WSU_News_Announcements {
 			$daywithpost = array();
 		}
 
-		if (strpos($_SERVER['HTTP_USER_AGENT'], 'MSIE') !== false || stripos($_SERVER['HTTP_USER_AGENT'], 'camino') !== false || stripos($_SERVER['HTTP_USER_AGENT'], 'safari') !== false)
+		if ( strpos( $_SERVER['HTTP_USER_AGENT'], 'MSIE' ) !== false || stripos( $_SERVER['HTTP_USER_AGENT' ], 'camino') !== false || stripos( $_SERVER['HTTP_USER_AGENT'], 'safari' ) !== false )
 			$ak_title_separator = "\n";
 		else
 			$ak_title_separator = ', ';
 
 		$ak_titles_for_day = array();
-		$ak_post_titles = $wpdb->get_results("SELECT ID, post_title, DAYOFMONTH(post_date) as dom "
+		$ak_post_titles = $wpdb->get_results( $wpdb->prepare( "SELECT ID, post_title, DAYOFMONTH(post_date) as dom "
 			."FROM $wpdb->posts "
 			."WHERE post_date >= '{$thisyear}-{$thismonth}-01 00:00:00' "
 			."AND post_date <= '{$thisyear}-{$thismonth}-{$last_day} 23:59:59' "
-			."AND post_type = 'post' AND post_status = 'publish'"
-		);
+			."AND post_type = %s AND post_status = 'publish'", $this->post_type ) );
+
 		if ( $ak_post_titles ) {
 			foreach ( (array) $ak_post_titles as $ak_post_title ) {
 
 				$post_title = esc_attr( apply_filters( 'the_title', $ak_post_title->post_title, $ak_post_title->ID ) );
 
-				if ( empty($ak_titles_for_day['day_'.$ak_post_title->dom]) )
-					$ak_titles_for_day['day_'.$ak_post_title->dom] = '';
-				if ( empty($ak_titles_for_day["$ak_post_title->dom"]) ) // first one
-				$ak_titles_for_day["$ak_post_title->dom"] = $post_title;
+				if ( empty( $ak_titles_for_day[ 'day_' . $ak_post_title->dom ] ) )
+					$ak_titles_for_day[ 'day_' . $ak_post_title->dom ] = '';
+				if ( empty( $ak_titles_for_day[ "$ak_post_title->dom" ] ) ) // first one
+					$ak_titles_for_day[ "$ak_post_title->dom" ] = $post_title;
 				else
-					$ak_titles_for_day["$ak_post_title->dom"] .= $ak_title_separator . $post_title;
+					$ak_titles_for_day[ "$ak_post_title->dom" ] .= $ak_title_separator . $post_title;
 			}
 		}
 
 		// See how much we should pad in the beginning
-		$pad = calendar_week_mod(date('w', $unixmonth)-$week_begins);
+		$pad = calendar_week_mod( date( 'w', $unixmonth ) - $week_begins );
 		if ( 0 != $pad )
-			$calendar_output .= "\n\t\t".'<td colspan="'. esc_attr($pad) .'" class="pad">&nbsp;</td>';
+			$calendar_output .= "\n\t\t" . '<td colspan="' . esc_attr( $pad ) . '" class="pad">&nbsp;</td>';
 
-		$daysinmonth = intval(date('t', $unixmonth));
+		$daysinmonth = intval( date( 't', $unixmonth ) );
 		for ( $day = 1; $day <= $daysinmonth; ++$day ) {
-			if ( isset($newrow) && $newrow )
+			if ( isset( $newrow ) && $newrow )
 				$calendar_output .= "\n\t</tr>\n\t<tr>\n\t\t";
 			$newrow = false;
 
-			if ( $day == gmdate('j', current_time('timestamp')) && $thismonth == gmdate('m', current_time('timestamp')) && $thisyear == gmdate('Y', current_time('timestamp')) )
+			if ( $day == gmdate( 'j', current_time( 'timestamp' ) ) && $thismonth == gmdate( 'm', current_time( 'timestamp' ) ) && $thisyear == gmdate( 'Y', current_time( 'timestamp' ) ) )
 				$calendar_output .= '<td id="today">';
 			else
 				$calendar_output .= '<td>';
 
-			if ( in_array($day, $daywithpost) ) // any posts today?
+			if ( in_array( $day, $daywithpost ) ) // any posts today?
 			$calendar_output .= '<a href="' . get_day_link( $thisyear, $thismonth, $day ) . '" title="' . esc_attr( $ak_titles_for_day[ $day ] ) . "\">$day</a>";
 			else
 				$calendar_output .= $day;
 			$calendar_output .= '</td>';
 
-			if ( 6 == calendar_week_mod(date('w', mktime(0, 0 , 0, $thismonth, $day, $thisyear))-$week_begins) )
+			if ( 6 == calendar_week_mod( date( 'w', mktime( 0, 0 , 0, $thismonth, $day, $thisyear ) ) - $week_begins ) )
 				$newrow = true;
 		}
 
-		$pad = 7 - calendar_week_mod(date('w', mktime(0, 0 , 0, $thismonth, $day, $thisyear))-$week_begins);
+		$pad = 7 - calendar_week_mod( date( 'w', mktime( 0, 0 , 0, $thismonth, $day, $thisyear ) ) - $week_begins );
 		if ( $pad != 0 && $pad != 7 )
-			$calendar_output .= "\n\t\t".'<td class="pad" colspan="'. esc_attr($pad) .'">&nbsp;</td>';
+			$calendar_output .= "\n\t\t" . '<td class="pad" colspan="' . esc_attr( $pad ) . '">&nbsp;</td>';
 
 		$calendar_output .= "\n\t</tr>\n\t</tbody>\n\t</table>";
 
 		$cache[ $key ] = $calendar_output;
-		wp_cache_set( 'get_calendar', $cache, 'calendar' );
+		wp_cache_set( $this->calendar_cache_key, $cache, $this->post_type );
 
 		if ( $echo )
-			echo apply_filters( 'get_calendar',  $calendar_output );
+			echo $calendar_output;
 		else
-			return apply_filters( 'get_calendar',  $calendar_output );
+			return $calendar_output;
 
+		return null;
 	}
 
 	/**
@@ -680,7 +689,7 @@ class WSU_News_Announcements {
 	 * @since 2.1.0
 	 */
 	public function delete_calendar_cache() {
-		wp_cache_delete( 'get_calendar', 'calendar' );
+		wp_cache_delete( $this->calendar_cache_key, $this->post_type );
 	}
 }
 $wsu_news_announcements = new WSU_News_Announcements();
