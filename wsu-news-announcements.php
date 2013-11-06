be518edfd3eaafefb97ffe539357937540de25f4
jeremyfelt <jeremy.felt@gmail.com>
2013-11-05 16:24:30 -0800

    Rename wsu-content-type-announcements to reflect singularity of the post type - announcement

--- wsu-news-announcements.php
+++ wsu-news-announcements.php
@@ -7,6 +7,6 @@ Author: washingtonstateuniversity, jeremyfelt
 Version: 0.9
 */
 
-include( dirname( __FILE__ ) . '/includes/wsu-content-type-announcements.php'        );
+include( dirname( __FILE__ ) . '/includes/wsu-content-type-announcement.php' );
 include( dirname( __FILE__ ) . '/includes/wsu-news-announcement-calendar-widget.php' );
 include( dirname( __FILE__ ) . '/includes/wsu-content-type-newsletter.php'           );
