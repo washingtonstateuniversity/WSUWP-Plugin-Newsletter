220f288f1b5d7ca04726a87800ef9e6d89811cd8
jeremyfelt <jeremy.felt@gmail.com>
2013-11-07 11:58:07 -0800

    Prepare our ajax request with the newsletter type
    The response will need to be used to fill out other options. In the case of announcements, we'll likely just grab a list of available announcements for filling everything in. In the case of news, we'll need to offer quite a bit more.

--- js/wsu-newsletter-admin.js
+++ js/wsu-newsletter-admin.js
@@ -1,6 +1,22 @@
 /**
  * Handle various features required in creation of newsletters in the admin.
  */
-( function( $ ) {
+( function( $, window ) {
 
-}( jQuery ) );
+	$( '.newsletter-type').on( 'click', function( e ) {
+		// Don't do anything rash.
+		e.preventDefault();
+
+		// Pass this.id to window.ajaxurl
+		var data = {
+			action: 'set_newsletter_type',
+			newsletter_type: this.id
+		};
+
+		// Make the ajax call
+		$.post( window.ajaxurl, data, function( response ) {
+			// Use this data to setup the remainder.
+			console.log( response );
+		});
+	});
+}( jQuery, window ) );
