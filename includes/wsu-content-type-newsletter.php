f2a1ccc84fa5286a90766cd6626ab1687706de43
jeremyfelt <jeremy.felt@gmail.com>
2013-11-07 11:50:02 -0800

    Enqueue the script in the footer

--- includes/wsu-content-type-newsletter.php
+++ includes/wsu-content-type-newsletter.php
@@ -107,7 +107,7 @@ class WSU_Content_Type_Newsletter {
 			return;
 
 		if ( $this->post_type === get_current_screen()->id )
-			wp_enqueue_script( 'wsu-newsletter-admin', plugins_url( 'js/wsu-newsletter-admin.js', dirname( __FILE__ ) ) );
+			wp_enqueue_script( 'wsu-newsletter-admin', plugins_url( 'js/wsu-newsletter-admin.js', dirname( __FILE__ ) ), false, false, true );
 	}
 
 	public function single_template( $template ) {
