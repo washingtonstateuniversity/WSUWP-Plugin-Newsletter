c3918dbc87aaaf2083930d4f4e321ed243417428
jeremyfelt <jeremy.felt@gmail.com>
2013-11-08 15:50:59 -0800

    Abstract functionality to load newsletter items into the build window
    Now that we're using this on page load, it makes sense to split it out in a way that can be used for multiple cases

--- js/wsu-newsletter-admin.js
+++ js/wsu-newsletter-admin.js
@@ -6,6 +6,27 @@
 	var $newsletter_build = $('#newsletter-build-items'),
 		sorted_data = [];
 
+	if ( window.wsu_newsletter.items instanceof Array ) {
+		load_newsletter_items( window.wsu_newsletter.items );
+	}
+
+	function load_newsletter_items( raw_data ) {
+		var data = '';
+
+		// Append the results to the existing build of newsletter items.
+		$.each( raw_data, function( index, val ) {
+			data += '<div id="newsletter-item-' + val.id + '" class="newsletter-item">' +
+				'<h3><a href="' + val.permalink + '">' + val.title + '</a></h3>' +
+				'<p>' + val.excerpt + ' <a href="' + val.permalink + '" >Continue reading&hellip;</a></p>' +
+				'</div>';
+		});
+
+		$newsletter_build.html( data );
+
+		// Use jQuery UI Sortable to add sorting functionality to newsletter items.
+		$newsletter_build.sortable( { axis: "y", opacity: 0.6, items: ".newsletter-item" } );
+	}
+
 	/**
 	 * As newsletter items are sorted in the newsletter build container, process
 	 * the associated post IDs into something that we can pass to the back end.
@@ -40,18 +61,7 @@
 			var data = '',
 				response_data = $.parseJSON( response );
 
-			// Append the results to the existing build of newsletter items.
-			$.each( response_data, function( index, val ) {
-				data += '<div id="newsletter-item-' + val.id + '" class="newsletter-item">' +
-					     '<h3><a href="' + val.permalink + '">' + val.title + '</a></h3>' +
-					     '<p>' + val.excerpt + ' <a href="' + val.permalink + '" >Continue reading&hellip;</a></p>' +
-					   '</div>';
-			});
-
-			$newsletter_build.html( data );
-
-			// Use jQuery UI Sortable to add sorting functionality to newsletter items.
-			$newsletter_build.sortable( { axis: "y", opacity: 0.6, items: ".newsletter-item" } );
+			load_newsletter_items( response_data );
 			process_sorted_data();
 		});
 	});
