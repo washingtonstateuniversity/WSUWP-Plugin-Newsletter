36228a73f3904cc4fa8f7ab1c2c6829e28968287
jeremyfelt <jeremy.felt@gmail.com>
2013-11-08 17:09:30 -0800

    Actually implement the massive HTML email template somewhat.
    Emails are now sending as HTML, though we're missing fancy background colors and such.

--- includes/wsu-content-type-newsletter.php
+++ includes/wsu-content-type-newsletter.php
@@ -244,9 +244,13 @@ class WSU_Content_Type_Newsletter {
 			exit;
 		}
 
-		$email_html = '<html><head><title>Newsletter</title></head><body></body></html>';
+		$email_html = $this->generate_html_email( $post_id, $post_ids );
+		$headers = "From: WSU Announcements\r\n";
+		$headers .= 'MIME-Version: 1.0' . "\r\n";
+		$headers .= 'Content-type: text/html; charset=UTF-8' . "\r\n";
+
 		add_filter( 'wp_mail_content_type', array( $this, 'set_mail_content_type' ) );
-		wp_mail( $_POST['email'], 'Announcements Newsletter', $email_html );
+		wp_mail( $_POST['email'], 'Announcements Newsletter', $email_html, $headers );
 		remove_filter( 'wp_mail_content_type', array( $this, 'set_mail_content_type' ) );
 
 		echo 'Emailed ' . esc_html( $_POST['email'] ) . '...';
@@ -278,6 +282,231 @@ class WSU_Content_Type_Newsletter {
 			return dirname( dirname( __FILE__ ) ) . '/templates/single-' . $this->post_type . '.php';
 		else
 			return $template;
+	}
+
+	private function generate_html_email( $post_id, $post_ids ) {
+		$email_title = esc_html( get_the_title( $post_id ) );
+		$newsletter_items = $this->_build_announcements_newsletter_response( $post_ids );
+
+		$html_email = <<<EMAIL
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+	<meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+	<title>$email_title</title>
+	<style type="text/css">
+		/* Based on The MailChimp Reset INLINE: Yes. */
+		/* Client-specific Styles */
+		#outlook a {padding:0;} /* Force Outlook to provide a "view in browser" menu link. */
+		body{width:100% !important; -webkit-text-size-adjust:100%; -ms-text-size-adjust:100%; margin:0; padding:0;}
+		/* Prevent Webkit and Windows Mobile platforms from changing default font sizes.*/
+		.ExternalClass {width:100%;} /* Force Hotmail to display emails at full width */
+		.ExternalClass, .ExternalClass p, .ExternalClass span, .ExternalClass font, .ExternalClass td, .ExternalClass div {line-height: 100%;}
+		/* Forces Hotmail to display normal line spacing.  More on that: http://www.emailonacid.com/forum/viewthread/43/ */
+		#background-full {
+			margin:0;
+			padding:0;
+			width:100% !important;
+			line-height: 100% !important;
+			background: #5e6a71;
+		}
+
+		#background-header {
+			margin: 0;
+			padding: 0;
+			width: 100%;
+			line-height: 36px;
+			background: #981e32;
+			color: #fff;
+			font-family: 'Lucida Grande', 'Lucida Sans Unicode', arial, helvetica, sans-serif;
+			font-size: 26px;
+		}
+
+		#background-message {
+			margin: 0;
+			padding: 0;
+			background: #fff;
+			font-family: 'Lucida Grande', 'Lucida Sans Unicode', arial, helvetica, sans-serif;
+			font-size: 12px;
+		}
+		/* End reset */
+
+		/* Some sensible defaults for images
+		Bring inline: Yes. */
+		img {outline:none; text-decoration:none; -ms-interpolation-mode: bicubic;}
+		a img {border:none;}
+		.image_fix {display:block;}
+
+		/* Yahoo paragraph fix
+		Bring inline: Yes. */
+		p {margin: 1em 0;}
+
+		/* Hotmail header color reset
+		Bring inline: Yes. */
+		h1 {
+			color: white !important;
+			font-size: 26px;
+		}
+
+		h2, h3, h4, h5, h6 {color: black !important;}
+
+		h1 a, h2 a, h3 a, h4 a, h5 a, h6 a {color: blue !important;}
+
+		h1 a:active, h2 a:active,  h3 a:active, h4 a:active, h5 a:active, h6 a:active {
+			color: red !important; /* Preferably not the same color as the normal header link color.  There is limited support for psuedo classes in email clients, this was added just for good measure. */
+		}
+
+		h1 a:visited, h2 a:visited,  h3 a:visited, h4 a:visited, h5 a:visited, h6 a:visited {
+			color: purple !important; /* Preferably not the same color as the normal header link color. There is limited support for psuedo classes in email clients, this was added just for good measure. */
+		}
+
+		/* Outlook 07, 10 Padding issue fix
+		Bring inline: No.*/
+		table td {border-collapse: collapse;}
+
+		/* Remove spacing around Outlook 07, 10 tables
+		Bring inline: Yes */
+		table { border-collapse:collapse; mso-table-lspace:0pt; mso-table-rspace:0pt; }
+
+		/* Styling your links has become much simpler with the new Yahoo.  In fact, it falls in line with the main credo of styling in email and make sure to bring your styles inline.  Your link colors will be uniform across clients when brought inline.
+		Bring inline: Yes. */
+		a {color: orange;}
+
+
+		/***************************************************
+		****************************************************
+		MOBILE TARGETING
+		****************************************************
+		***************************************************/
+		@media only screen and (max-device-width: 480px) {
+			/* Part one of controlling phone number linking for mobile. */
+			a[href^="tel"], a[href^="sms"] {
+				text-decoration: none;
+				color: blue; /* or whatever your want */
+				pointer-events: none;
+				cursor: default;
+			}
+
+			.mobile_link a[href^="tel"], .mobile_link a[href^="sms"] {
+				text-decoration: default;
+				color: orange !important;
+				pointer-events: auto;
+				cursor: default;
+			}
+
+		}
+
+		/* More Specific Targeting */
+
+		@media only screen and (min-device-width: 768px) and (max-device-width: 1024px) {
+			/* You guessed it, ipad (tablets, smaller screens, etc) */
+			/* repeating for the ipad */
+			a[href^="tel"], a[href^="sms"] {
+				text-decoration: none;
+				color: blue; /* or whatever your want */
+				pointer-events: none;
+				cursor: default;
+			}
+
+			.mobile_link a[href^="tel"], .mobile_link a[href^="sms"] {
+				text-decoration: default;
+				color: orange !important;
+				pointer-events: auto;
+				cursor: default;
+			}
+		}
+
+		@media only screen and (-webkit-min-device-pixel-ratio: 2) {
+			/* Put your iPhone 4g styles in here */
+		}
+
+		/* Android targeting */
+		@media only screen and (-webkit-device-pixel-ratio:.75){
+			/* Put CSS for low density (ldpi) Android layouts in here */
+		}
+		@media only screen and (-webkit-device-pixel-ratio:1){
+			/* Put CSS for medium density (mdpi) Android layouts in here */
+		}
+		@media only screen and (-webkit-device-pixel-ratio:1.5){
+			/* Put CSS for high density (hdpi) Android layouts in here */
+		}
+		/* end Android targeting */
+
+	</style>
+
+	<!-- Targeting Windows Mobile -->
+	<!--[if IEMobile 7]>
+	<style type="text/css">
+
+	</style>
+	<![endif]-->
+
+	<!-- ***********************************************
+	****************************************************
+	END MOBILE TARGETING
+	****************************************************
+	************************************************ -->
+
+	<!--[if gte mso 9]>
+	<style>
+		/* Target Outlook 2007 and 2010 */
+	</style>
+	<![endif]-->
+</head>
+<body>
+<table cellpadding="0" cellspacing="0" border="0" id="background-full">
+	<tr>
+		<td valign="top">
+			<!-- Establish a header section at the top and center the title in a specific area -->
+			<table cellpadding="0" cellspacing="0" border="0" align="center" id="background-header">
+				<tr>
+					<td>
+						<table align="center">
+							<tr>
+								<td width="600" valign="top"><h1><?php the_title(); ?></h1></td>
+							</tr>
+						</table>
+					</td>
+				</tr>
+			</table>
+		</td>
+	</tr>
+	<tr>
+		<td valign="top">
+			<table cellpadding="0" cellspacing="0" border="0" align="center">
+				<tr><td>&nbsp;</td></tr>
+				<tr>
+					<td>
+						<table cellpadding="0" cellspacing="0" border="0" align="center" id="background-message">
+							<tr>
+								<td width="600" valign="top">
+								<p>Submit announcements online at <a href="http://news.wsu.edu/announcements/">http://news.wsu.edu/announcements</a></p>
+EMAIL;
+
+		foreach ( $newsletter_items as $item ) {
+			$html_email .= '<h3><a href="' . esc_url( $item['permalink'] ) . '">' . esc_html( $item['title'] ) . '</a></h3>';
+			$html_email .= '<p>' . esc_html( $item['excerpt'] ) . ' <a href="' . esc_url( $item['permalink'] ) . '">Continue reading&hellip;</a></p>';
+		}
+
+		$html_email .= <<<EMAIL
+									<p>The Announcement newsletter will continue to be sent once a day at 10 a.m. Submissions made after 9 a.m. each day will appear in the next days’ newsletter. Any edits will be still be made by Brenda Campbell at <a href="mailto:bcampbell@wsu.edu">bcampbell@wsu.edu</a>.</p>
+									<p>If you are having difficulty reading the announcements, try unsubscribing and then resubscribe. Click <a href="http://lists.wsu.edu/leave.php">here</a> to unsubscribe and <a href="http://lists.wsu.edu/join.php">here</a> to subscribe</p>
+								</td>
+							</tr>
+						</table>
+					</td>
+				</tr>
+				<tr><td>&nbsp;</td></tr>
+			</table>
+		</td>
+	</tr>
+</table>
+<!-- End of wrapper table -->
+</body>
+</html>
+EMAIL;
+		return $html_email;
 	}
 }
 $wsu_content_type_newsletter = new WSU_Content_Type_Newsletter();
