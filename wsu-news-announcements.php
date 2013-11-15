2582f87b78483c5de345115ed69f6f69d96ab5d7
jeremyfelt <jeremy.felt@gmail.com>
2013-11-15 12:30:06 -0800

    Tag v1.4 for push to production

--- wsu-news-announcements.php
+++ wsu-news-announcements.php
@@ -4,7 +4,7 @@ Plugin Name: WSU News & Announcements
 Plugin URI: http://news.wsu.edu/
 Description: Handles functionality for WSU News and Announcements
 Author: washingtonstateuniversity, jeremyfelt
-Version: 1.3
+Version: 1.4
 */
 
 include( dirname( __FILE__ ) . '/includes/wsu-content-type-announcement.php' );
